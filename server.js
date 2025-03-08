07bb4570cb4279ce5a241f9c5238a8ad18826b93
clocktower39 <clocktower39@aol.com>
2025-03-07 21:58:57 -0700

    socket.io online status integrated

--- server.js
+++ server.js
@@ -1,49 +1,77 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-const http = require('http').Server(app);
-const mongoose = require('mongoose');
+const http = require("http").Server(app);
+const mongoose = require("mongoose");
 const { ValidationError } = require("express-validation");
-const cors = require('cors');
-require('dotenv').config();
-global.io = require('./io').initialize(http, {
+const cors = require("cors");
+require("dotenv").config();
+global.io = require("./io").initialize(http, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
-    credentials: true
-  }
+    credentials: true,
+  },
 });
-const userRoutes = require('./routes/userRoutes');
-const messageRoutes = require('./routes/messageRoutes');
+const userRoutes = require("./routes/userRoutes");
+const messageRoutes = require("./routes/messageRoutes");
 
 const dbUrl = process.env.DBURL;
 
 let PORT = process.env.PORT;
-if( PORT == null || PORT == ""){
-    PORT = 8000;
+if (PORT == null || PORT == "") {
+  PORT = 8000;
 }
 
-
-app.use(cors())
+app.use(cors());
 app.use(express.static(__dirname));
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use('/', userRoutes);
-app.use('/', messageRoutes);
+app.use("/", userRoutes);
+app.use("/", messageRoutes);
 
-let corsWhitelist = ['*'];
+let corsWhitelist = ["*"];
 var corsOptions = {
   origin: function (origin, callback) {
     if (corsWhitelist.indexOf(origin) !== -1) {
-      callback(null, true)
+      callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'))
+      callback(new Error("Not allowed by CORS"));
     }
-  }
-}
+  },
+};
 
-global.io.on('connection', (socket) => {
-    console.log('a user connected')
+const connectedClients = {};
+
+global.io.on("connection", (socket) => {
+  const userId = socket.handshake.query.userId;
+
+  // Save the user's socket ID
+  connectedClients[userId] = socket.id;
+
+  // Notify all trainers about the client's online status
+  global.io.emit("clientStatusChanged", { userId, status: "online" });
+
+  console.log(`${userId} connected with IP: ${socket.conn.remoteAddress}`);
+
+  // Listen for the 'requestClientStatuses' event and send the current status
+  socket.on("requestClientStatuses", () => {
+    const clientStatuses = Object.keys(connectedClients).reduce((statuses, id) => {
+      statuses[id] = "online";
+      return statuses;
+    }, {});
+    socket.emit("currentClientStatuses", clientStatuses);
+  });
+
+  socket.on("disconnect", () => {
+    // Remove the client from the connectedClients object
+    delete connectedClients[userId];
+
+    // Notify all trainers about the client's offline status
+    global.io.emit("clientStatusChanged", { userId, status: "offline" });
+
+    console.log(`${userId} disconnected`);
+  });
 });
 
 const connectToDB = async () => {
@@ -66,7 +94,6 @@ app.use((err, req, res, next) => {
   res.status(500).send(err.stack);
 });
 
-let server = http.listen(PORT, ()=> {
-    console.log(`Server is listening on port ${PORT}`);
+let server = http.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
-
