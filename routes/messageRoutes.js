998438f43cb532cd3b0bdd98cbbb795d1af76e09
clocktower39 <clocktower39@aol.com>
2025-03-15 23:51:21 -0700

    auth middleware fix

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const messageController = require('../controllers/messageController');
-const auth = require("../middleware/auth");
+const { verifyAccessToken, verifyRefreshToken } = require("../middleware/auth");
 
 const router = express.Router();
 
-router.get('/messages', auth, messageController.get_messages);
-router.post('/messages', auth, messageController.post_message);
-router.post('/deleteMessage', auth, messageController.delete_message);
+router.get('/messages', verifyAccessToken, messageController.get_messages);
+router.post('/messages', verifyAccessToken, messageController.post_message);
+router.post('/deleteMessage', verifyAccessToken, messageController.delete_message);
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const userController = require('../controllers/userController');
-const auth = require("../middleware/auth");
+const { verifyAccessToken, verifyRefreshToken } = require("../middleware/auth");
 const { uploadProfilePicture } = require("../mygridfs");
 
 const router = express.Router();
 
-router.get('/checkAuthToken', auth, userController.checkAuthLoginToken);
-router.get('/users', auth, userController.get_user_list);
+router.get('/users', verifyAccessToken, userController.get_user_list);
 router.get('/user/image/:id', userController.get_profile_picture);
-router.get('/user/remove/image/', auth, userController.delete_profile_picture);
-router.post('/user/image/upload', auth, uploadProfilePicture.single("file"), userController.upload_profile_picture);
+router.get('/user/remove/image/', verifyAccessToken, userController.delete_profile_picture);
+router.post('/user/image/upload', verifyAccessToken, uploadProfilePicture.single("file"), userController.upload_profile_picture);
 router.post('/login', userController.login_user);
 router.post('/signup', userController.signup_user);
 
